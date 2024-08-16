45c57316463cb29188573aed803a6201eaaca358
Juanzor <hjuan7217@gmail.com>
2024-08-16 19:37:07 -0300

    ultimos cambios

--- index.js
+++ index.js
@@ -8,37 +8,36 @@ let quantity = 6;
 
 const generateColor = () => {
     let color = '#';
-
-    for (let i = 0; i < quantity; i++) {
-        for (let i = 0; i < 6; i++) {
-            color += Math.floor(Math.random() * 16).toString(16);
+    for (let i = 0; i < quantity; i++) { // Genera la cantidad de cajas correspondientes 
+        for (let i = 0; i < 6; i++) {                             // Genera 6 veces un numero al azar entre 0 
+            color += Math.floor(Math.random() * 16).toString(16); // y 15, y toString() los convierte a su valor hexadecimal
         }
         return color;
     }
 };
 
-const generateArrayColors = () => {
+const generateArrayColors = () => { // Crea el array con la cantidad de cajas correspondientes usando generateColors() y asegura que no haya duplicados
     while (colors.length < quantity) {
-        const nuevoColor = generateColor();
-        if (!colors.includes(nuevoColor)) {
-            colors.push(nuevoColor);
+        const newColor = generateColor();
+        if (!colors.includes(newColor)) {
+            colors.push(newColor);
         }
     }
 };
 
 generateArrayColors();
-colorToGuess = colors[Math.floor(Math.random() * quantity)];
+colorToGuess = colors[Math.floor(Math.random() * quantity)]; // Retorna una posicion al azar de colors[] mediante un indice aleatorio entre 0 y quantity
 
 guess.textContent = `El color a adivinar es ${colorToGuess}`;
 
-const verifyColor = (color) => {
+const verifyColor = (color) => {  // Verifica si  el color tomado al azar de colors[] es igual al color pasado por parametro
     if (color === colorToGuess) {
         window.alert('Ganaste!! Reiniciando...');
         window.location.reload();
     }
 };
 
-const renderColors = () => {
+const renderColors = () => { // Renderiza cada caja con su color, le asigna su valor mediante el id y su listener verifyColor
     colors.forEach((e) => {
         const box = document.createElement('div');
         box.classList.add('box');
